c782139445512559b8fa51bfac2ffeb2ffb0bf3c
zamilnugraha <zamil.nugraha@gmail.com>
2019-08-09 10:54:37 +0700

    add promosi ROM to GMO

--- admin/promosi_act_rom.php
+++ admin/promosi_act_rom.php
@@ -3,57 +3,25 @@ session_start();
 include 'config.php';
 
 $id=$_POST['nik'];
-$nama=$_POST['namaAM'];
-$email=$_POST['emailAM'];
+$nama=$_POST['namaROM'];
+$email=$_POST['emailROM'];
 $rsc=$_POST['rsc'];
-$region=$_POST['region'];
 
-//validation insert status region
-$cekRegion = mysql_query("SELECT ith_usergroup_region.status_vacant,ith_usergroup_region.usergroup_aliasname  FROM ith_usergroup_region
-                            WHERE ith_usergroup_region.usergroup_kd ='$region'");
-$getStatusRegion = mysql_fetch_array($cekRegion);
-$statusVacant = $getStatusRegion['status_vacant'];
-$nameAlias = $getStatusRegion['usergroup_aliasname'];
+$data = mysql_query("SELECT ith_user.usergroup_id FROM ith_user WHERE user_nik = '$id'");
+$tampung = mysql_fetch_array($data);
+$region = $tampung['usergroup_id'];
 
-if(strlen($region) > 4){
-    $udeptUserGroup = substr($region,-2,2);
-}else{
-    $udeptUserGroup = substr($region,-2);
-}
-$getLastChar = str_replace(-1,"",$udeptUserGroup);
-$udeptId = "REGIONAL_MANAGER_".$getLastChar;
-$namaJabatan = "ROM".$getLastChar."(".$nameAlias.")";
+$sql = mysql_query("UPDATE ith_user SET udept_id='GENERAL MANAGER', userlevel_id='100', nik_atasan='', nama_atasan='DIREKSI', email_atasan='',
+        nama_jabatan ='G.M.OPERATION', user_divheadname='', user_deptheadname='', email_divhead='', userrsc_code='$rsc', usergroup_id='', usersubgroup_id='', 
+        departemen_id='' WHERE user_nik='$id'");
+        
+//Update table ith_usergroup_region
+mysql_query("UPDATE ith_usergroup_region SET status_vacant = 'Yes' WHERE usergroup_kd = '$region'");       
 
-//Untuk mencari DivHead
-$sqlDiv = mysql_query("SELECT user_nik, user_realname, user_email FROM ith_user WHERE userdepartemen_id = 'GM OPERATION' AND userlevel_id = '1000' ");
-$CekNamaDiv = mysql_fetch_array($sqlDiv);
-$nikDiv = $CekNamaDiv['user_nik'];
-$namaDiv = $CekNamaDiv['user_realname'];
-$emailDiv = $CekNamaDiv['user_email'];
-
-//FOR userunit_id AM (Area Manager)
-$userUnitAM = mysql_query("SELECT ith_userunit.userunit_id FROM ith_userunit
-                            WHERE ith_userunit.userrsc_code = '$rsc' AND ith_userunit.userunit_name LIKE 'STORE%'");
-$getUserUnitAM = mysql_fetch_array($cekRegion);
-$userUnitId = $getUserUnitAM['userunit_id'];
-
-
-if($statusVacant == 'Yes'){
-    $sql = mysql_query("UPDATE ith_user SET user_realname='$nama', user_email='$email', udept_id='$udeptId', userlevel_id='8', nik_atasan='$nikDiv', nama_atasan='$namaDiv', email_atasan='$emailDiv',
-            nama_jabatan ='$namaJabatan', user_divheadname='DIREKSI', user_deptheadname='$namaDiv', email_divhead='$emailDiv', userrsc_code='$rsc', usergroup_id='$region', usersubgroup_id='', 
-            departemen_id='5', lantai='$lantai', handphone='$noHp', telpon='$telephone', userunit_id='$userUnitId'
-            WHERE user_nik='$id'");
-            
-    //Update table ith_usergroup_region
-    mysql_query("UPDATE ith_usergroup_region SET status_vacant = 'Yes' WHERE usergroup_kd = '$region'");        
-}else{
-    $_SESSION['area']='Area ini belum kosong, silahkan pilih yang area lain...';
-}
-
-header("location:am.php");
+header("location:rom.php");
 
 if($sql){
-    $_SESSION['success']=' Area Manager ' .$nama. ' berhasil dipromosi ';
+    $_SESSION['success']=' Regional Manager ' .$nama. ' berhasil dipromosikan ';
 }else{
     $_SESSION['error'] = "Mysql Error : ".mysql_error();
 }
--- admin/promosi_rom.php
+++ admin/promosi_rom.php
@@ -16,11 +16,11 @@ while($d=mysql_fetch_array($det)){
 			</tr>
 			<tr>
 				<td>Nama Regional Manager</td>
-				<td><input type="text" class="form-control" name="namaAM" value="<?php echo $d['user_realname'] ?>" readonly></td>
+				<td><input type="text" class="form-control" name="namaROM" value="<?php echo $d['user_realname'] ?>" readonly></td>
 			</tr>
 			<tr>
 				<td>Email Regional Manager</td>
-				<td><input type="text" class="form-control" name="emailAM" value="<?php echo $d['user_email'] ?>" readonly></td>
+				<td><input type="text" class="form-control" name="emailROM" value="<?php echo $d['user_email'] ?>" readonly></td>
 			</tr>
 			<tr>
 				<td>RSC Store</td>
@@ -36,32 +36,6 @@ while($d=mysql_fetch_array($det)){
 						?>
 					</select>
 				</td>
-			</tr>
-			<tr>
-				<script type="text/javascript" src="jquery-1.4.2.min.js"></script>
-				<td>Region</td>
-				<td>
-					<select name="region" id="region" class="form-control">
-						<option value="">- Pilih Region Store -</option>
-					
-						<!-- looping data region -->
-						<?php
-							$sel_prov="SELECT ith_usergroup_region.usergroup_kd,ith_usergroup_region.usergroup_aliasname, ith_usergroup_region.status_vacant FROM ith_usergroup_region";
-							$q=mysql_query($sel_prov);
-							while($data_prov=mysql_fetch_array($q)){
-							
-							if($data_prov["status_vacant"] == "No"){
-								$output = "Not Vacant";
-							}else{
-								$output = "Vacant";
-							}
-						?>
-							<option <?php if($d['usergroup_id']==$data_prov['usergroup_kd']){echo "selected"; } ?> value="<?php echo $data_prov["usergroup_kd"] ?>"><?php echo $data_prov["usergroup_aliasname"] ?> (<?php echo $output;?>)</option>
-						<?php
-							}
-						?>
-					</select>
-				</td> 
 			</tr>
 			<tr>
 				<td></td>
